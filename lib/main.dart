450d2da0f4441253f8f91856bd2ef6f9d1e755b9
Volodymir Kutysch <dsuperdeveloper777@gmail.com>
2023-06-02 15:13:32 -0400

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -52,7 +52,8 @@ class MyApp extends StatelessWidget {
                     whiteColor,
                     whiteColor,
                     true),
-                home: const LandingPage(),
+                // home: const LandingPage(),
+                home: const SplashScreen(),
               ),
             );
           });
